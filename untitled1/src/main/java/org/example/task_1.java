3dab82b845bcfb34c868f539d8497ee443f48d0e
Fling <Nollkol560@gmail.com>
2022-11-08 16:45:57 +0500

    new_file_@222

---
+++ untitled1/src/main/java/org/example/task_1.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ Реализовать алгоритм сортировки слиянием.
+*/
+public class task_1 {
+
+    public static void main(String[] args) {
+        int[] listNum = {5, 6, 8, 2, 3, 1, 4};
+
+    }
+
+
+}
+
---
+++ untitled1/src/main/java/org/example/task_2.java
@@ -0,0 +1,32 @@
+package org.example;
+
+
+import java.util.ArrayList;
+
+/**
+ Пусть дан произвольный список целых чисел. Удалить из него чётные числа.
+ */
+
+
+public class task_2 {
+    public static void main(String[] args) {
+        ArrayList <Integer> listNum = new ArrayList<>();
+        ArrayList <Integer> resultNum = new ArrayList<>();
+
+        int dip = 100;
+
+        for (int i = 0; i < dip + 1; i++){
+            int randNum = (int) ( Math.random() * dip );
+            listNum.add(randNum);
+        }
+        for (int i : listNum){
+            if (i % 2 != 0 ){
+                resultNum.add(i);
+            }
+        }
+        System.out.println(listNum);
+        System.out.println(resultNum);
+
+
+    }
+}
