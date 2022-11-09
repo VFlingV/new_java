c6bfc409f7b768aac60accd55c6bf77fce3944fc
Fling <Nollkol560@gmail.com>
2022-11-09 12:30:31 +0500

    lesson_4_1

--- untitled1/src/main/java/org/example/task_2.java
+++ untitled1/src/main/java/org/example/task_2.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  * Реализуйте очередь с помощью LinkedList со следующими методами:
  * enqueue() - помещает элемент в конец очереди, dequeue() - возвращает первый элемент из очереди и удаляет его,
  * first() - возвращает первый элемент из очереди, не удаляя.
- *
+ *2
  */
 public class task_2 {
     public static void main(String[] args) {
